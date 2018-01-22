3b885f61392a47486fe18bef6cad2d994f190d7c
DESKTOP-TCRB84H\Jared <uhhcs@hawaii.edu>
2018-01-22 12:16:53 -1000

    process each object

--- src/app/map/shared/dbconnect.service.ts
+++ src/app/map/shared/dbconnect.service.ts
@@ -9,27 +9,34 @@ export class DBConnectService {
 
   constructor(private http: HttpClient) { }
 
+  generateCover(cover: string, data) {
+    //modify cover file
+    //might want to create multiples to have variable color hues
+    //have empty space by only including coordinates containing data for that landcover, then load multiple layers with different hues
+    //that is, return collection of files from this method
+  }
 
 
-  spatialSearch(drawnItems) {
-    var oAuthAccessToken = "25ca1117301984b2ec79ce76f8ed1df7";
+  spatialSearch(drawnItems, size: number) {
+    var oAuthAccessToken = "token";
     //this will use the 0 indexed feature from the leaflet map, which should be a GeoJSON polygon, for the spatial search boundry
-    var query = "{'$and':[{'name':'Landuse'},{'value.name':'testunit3'},{'value.loc': {$geoWithin: {'$geometry':"+JSON.stringify(drawnItems.toGeoJSON().features[0].geometry).replace(/"/g,'\'')+"}}}]}";
+    for(let i = 0; i < size; i++) {
+      var query = "{'$and':[{'name':'Landuse'},{'value.name':'testunit3'},{'value.loc': {$geoWithin: {'$geometry':"+JSON.stringify(drawnItems.toGeoJSON().features[0].geometry).replace(/"/g,'\'')+"}}}]}";
 
-    var url = "https://agaveauth.its.hawaii.edu:443/meta/v2/data?q="+encodeURI(query)+"&limit=10000&offset=0";
-    var head = new HttpHeaders()
-    .set("Authorization", "Bearer " + oAuthAccessToken)
-    .set("Content-Type", "application/x-www-form-urlencoded");
-    var options = {
-      headers: head
-    };
-    this.http.get(url, options).subscribe((data) => {
-      alert(JSON.stringify(data));
-    },
-    (err) => {
-      alert("fail")
+      var url = "https://agaveauth.its.hawaii.edu:443/meta/v2/data?q="+encodeURI(query)+"&limit=10000&offset=0";
+      var head = new HttpHeaders()
+      .set("Authorization", "Bearer " + oAuthAccessToken)
+      .set("Content-Type", "application/x-www-form-urlencoded");
+      var options = {
+        headers: head
+      };
+      this.http.get(url, options).subscribe((data) => {
+        //alert(JSON.stringify(data));
+      },
+      (err) => {
+          //alert("fail")
+      });
     }
-  );
 
 
     // $.ajax({
