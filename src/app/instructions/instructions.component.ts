cd319c1849b3a8bc1716657f021fc135d2b96152
princessbobsanta <princessbobsantaclaus@gmail.com>
2019-01-08 01:09:42 -1000

    instructions with image panel, want to update image panel to use tip in link

--- src/app/instructions/instructions.component.ts
+++ src/app/instructions/instructions.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import {animate, transition, state, trigger, style} from '@angular/animations';
+
 
 @Component({
   selector: 'app-instructions',
   templateUrl: './instructions.component.html',
-  styleUrls: ['./instructions.component.css']
+  styleUrls: ['./instructions.component.css'],
+  animations: [
+    trigger('toggle', [
+      state("Hide Image", style({
+        height: "{{expanded}}px",
+      }),
+      {params: {expanded: "0px"}}),
+      state("Show Image", style({
+        height: "{{retracted}}px",
+      }),
+      {params: {retracted: "0px"}}),
+      transition("* => *", animate('500ms ease-in-out')),
+    ])
+  ]
 })
+
 export class InstructionsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('imageContainer') imageContainer;
+  @ViewChild('image') image;
+  @ViewChild('ref') ref;
+  @ViewChild('arrow') arrow;
+
+  toggleMessage = "Hide Image";
+
+  mouseDown = false;
+  scrolled = false;
+  expandHeight = 118 + 25;
+  retractHeight = 0;
+
+  constructor(private changeDetector: ChangeDetectorRef) { }
+
+  initHeight() {
+    // this.expandHeight = Math.max(window.pageYOffset + this.image.nativeElement.offsetHeight - 75, this.image.nativeElement.offsetHeight + 25);
+    // this.changeDetector.detectChanges()
+    
+  }
 
   ngOnInit() {
+    this.retractHeight = Math.max(window.pageYOffset - 125, 0);
+
+    document.addEventListener('mousedown', () => {
+      this.mouseDown = true;
+    });
+
+    document.addEventListener('mouseup', () => {
+      this.mouseDown = false;
+      if(this.scrolled) {
+        this.expandHeight = Math.max(window.pageYOffset + this.image.nativeElement.offsetHeight - 75, this.image.nativeElement.offsetHeight + 25);
+        this.retractHeight = Math.max(window.pageYOffset - 125, 0);
+        this.scrolled = false;
+      }
+    });
+
+    document.addEventListener('scroll', (e) => {
+      if(this.toggleMessage == "Show Image" || !this.mouseDown) {
+        this.expandHeight = Math.max(window.pageYOffset + this.image.nativeElement.offsetHeight - 75, this.image.nativeElement.offsetHeight + 25);
+        this.retractHeight = Math.max(window.pageYOffset - 125, 0);
+      }
+      else {
+        this.scrolled = true;
+      }
+    });
+
+    window.addEventListener("resize", () => {
+      this.expandHeight = Math.max(window.pageYOffset + this.image.nativeElement.offsetHeight - 75, this.image.nativeElement.offsetHeight + 25);
+        this.retractHeight = Math.max(window.pageYOffset - 125, 0);
+    });
+  }
+
+  hover(src) {
+    this.image.nativeElement.setAttribute("src", src);
+    this.ref.nativeElement.setAttribute("href", src);
+    this.expandHeight = Math.max(window.pageYOffset + this.image.nativeElement.offsetHeight - 75, this.image.nativeElement.offsetHeight + 25);
+    this.retractHeight = Math.max(window.pageYOffset - 125, 0);
+  }
+
+  toggleImage() {
+    if(this.toggleMessage == "Hide Image") {
+      this.image.nativeElement.style.visibility = "hidden";
+      this.imageContainer.nativeElement.style.position = "absolute";
+      this.arrow.nativeElement.innerHTML = "&#187;";
+      this.toggleMessage = "Show Image";
+    }
+    else {
+      this.image.nativeElement.style.visibility = "visible";
+      this.imageContainer.nativeElement.style.position = "relative";
+      this.arrow.nativeElement.innerHTML = "&#171;";
+      this.toggleMessage = "Hide Image";
+    }
   }
 
 }
