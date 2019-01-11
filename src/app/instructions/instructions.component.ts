a7a3b8bed545bfe87488e7e2e982cba379fa2fc0
jardmclan <mcleanj@hawaii.edu>
2019-01-10 14:17:06 -1000

    instructions floating image working!

--- src/app/instructions/instructions.component.ts
+++ src/app/instructions/instructions.component.ts
@@ -54,23 +54,44 @@ export class InstructionsComponent implements OnInit {
 
   noScrollEl: HTMLStyleElement;
 
-  test = 0;
+  //scrolling = true;
+  scrollEnd = true;
+  endCB = null;
+  //imgChanged = false;
 
 
   constructor(private changeDetector: ChangeDetectorRef) { }
 
   updatePosition() {
-    
-    if(!this.bounce) {
-      requestAnimationFrame(() => {
-        //this.currentScrollPos = window.pageYOffset;
-        let height = Math.max(window.pageYOffset - 125, 0);
+    if(this.image.nativeElement.offsetHeight > 0) {
+      this.imgHeight = this.image.nativeElement.offsetHeight;
+      this.imgOffset = this.imgHeight + 10;
+    }
+    requestAnimationFrame(() => {
+      if(this.scrollEnd || this.toggleMessage == "Show Image") {
+        this.currentScrollPos = window.pageYOffset;
+        let height = Math.max(this.currentScrollPos - 125, 0);
         this.heightBalancer.nativeElement.style.height = height + 'px';
         this.buttonContainer.nativeElement.style.top = height + this.imgHeight - 15 + 'px';
-        this.updatePosition();
-      })
-    }
-    this.bounce = !this.bounce;
+        //this.imgChanged = false;
+
+        if(this.currentScrollPos != window.pageYOffset) {
+          window.scrollTo(window.pageXOffset, this.currentScrollPos);
+        }
+      }
+
+      
+      
+      //this.bounce = !this.bounce;
+      
+      // setTimeout(() => {
+      //   
+      //   this.updatePosition();
+      // }, throttle);
+      this.updatePosition();
+    });
+    //}
+    //this.bounce = !this.bounce;
   }
 
   ngOnInit() {
@@ -79,7 +100,14 @@ export class InstructionsComponent implements OnInit {
     this.imgHeight = 182;
     this.imgOffset = 190;
 
-    //this.updatePosition();
+    // requestAnimationFrame(() => {
+    //   this.hover("../assets/instruction_images/lc_vis.PNG");
+    this.updatePosition();
+    // });
+
+    //let throttlePeriod = 
+
+    
 
     //this.noScrollEl = this.buildStyleElement()
 
@@ -91,70 +119,95 @@ export class InstructionsComponent implements OnInit {
     // });
     // bounceSentinal.observe(document, {attributes: true , childList: true, subtree: true});
 
+    let endThreshold = 200
     document.addEventListener('scroll', (e) => {
-      
-      // if(e.timeStamp - this.lastTimestamp < 10) {
-      //   this.lastTimestamp = e.timeStamp;
-      //   return;
-      // }
-      // this.lastTimestamp = e.timeStamp;
-      // if(!this.bounce) {
-      if(this.skip) {
-        this.skip = false;
-        return;
+      this.scrollEnd = false;
+      if(this.endCB != null) {
+        clearTimeout(this.endCB);
       }
+      this.endCB = setTimeout(() => {
+        this.scrollEnd = true;
+        this.endCB = null;
+      }, endThreshold);
 
-      if(!this.bounce) {
-        let test = window.pageYOffset;
-        setTimeout(() => {
-          console.log(test - window.pageYOffset)
-        }, 100);
-        console.log(this.currentScrollPos - window.pageYOffset)
-        //console.log(this.test++);
-        this.bounce = true;
-
-        this.currentScrollPos = window.pageYOffset;
-
-        let height = Math.max(this.currentScrollPos - 125, 0);
-        this.heightBalancer.nativeElement.style.height = height + 'px';
-        this.buttonContainer.nativeElement.style.top = height + this.imgHeight - 15 + 'px';
-        // });
-        
-        //console.log(window.pageYOffset < this.currentScrollPos)
-        if(window.pageYOffset != this.currentScrollPos) {
-          // requestAnimationFrame(() => {
-          //   console.log("Test");
-          // });
-          this.skip = true;
-          //console.log("end");
-          this.bounce = false;
-        }
-        else {
-          this.bounce = false;
-        }
-        // else {
-        //   console.log("end");
-        //   this.bounce = false;
-        // }
-        // this.bounce = false;
-        //document.body.removeChild(this.noScrollEl);
-        // setTimeout(() => {
-        //   this.bounce = false;
-        // }, 1000);
-        
-      }
-      
-      // }
     });
+
+    // let observer = new MutationObserver((mutations, observer) => {
+    //   if(this.toggleMessage == "Hide Image") {
+    //     let imghDelta = this.image.nativeElement.offsetHeight - this.imgHeight;
+    //     this.imgHeight = this.image.nativeElement.offsetHeight;
+    //     this.imgOffset = this.imgHeight + 10;
+    //     this.buttonContainer.nativeElement.style.top = parseInt(this.buttonContainer.nativeElement.style.top) + imghDelta + 'px';
+    //   }
+    // });
+
+    // observer.observe(this.image.nativeElement, {attributes: true});
+
+    // document.addEventListener('scroll', (e) => {
+      
+    //   // if(e.timeStamp - this.lastTimestamp < 10) {
+    //   //   this.lastTimestamp = e.timeStamp;
+    //   //   return;
+    //   // }
+    //   // this.lastTimestamp = e.timeStamp;
+    //   // if(!this.bounce) {
+    //   if(this.skip) {
+    //     this.skip = false;
+    //     return;
+    //   }
+
+    //   if(!this.bounce) {
+    //     let test = window.pageYOffset;
+    //     setTimeout(() => {
+    //       console.log(test - window.pageYOffset)
+    //     }, 100);
+    //     console.log(this.currentScrollPos - window.pageYOffset)
+    //     //console.log(this.test++);
+    //     this.bounce = true;
+
+    //     this.currentScrollPos = window.pageYOffset;
+
+    //     let height = Math.max(this.currentScrollPos - 125, 0);
+    //     this.heightBalancer.nativeElement.style.height = height + 'px';
+    //     this.buttonContainer.nativeElement.style.top = height + this.imgHeight - 15 + 'px';
+    //     // });
+        
+    //     //console.log(window.pageYOffset < this.currentScrollPos)
+    //     if(window.pageYOffset != this.currentScrollPos) {
+    //       // requestAnimationFrame(() => {
+    //       //   console.log("Test");
+    //       // });
+    //       this.skip = true;
+    //       //console.log("end");
+    //       this.bounce = false;
+    //     }
+    //     else {
+    //       this.bounce = false;
+    //     }
+    //     // else {
+    //     //   console.log("end");
+    //     //   this.bounce = false;
+    //     // }
+    //     // this.bounce = false;
+    //     //document.body.removeChild(this.noScrollEl);
+    //     // setTimeout(() => {
+    //     //   this.bounce = false;
+    //     // }, 1000);
+        
+    //   }
+      
+    //   // }
+    // });
 
     window.addEventListener("resize", () => {
       if(this.toggleMessage == "Hide Image") {
-        this.imgHeight = this.image.nativeElement.offsetHeight;
-        this.imgOffset = this.imgHeight + 10;
+        let height = Math.max(this.currentScrollPos - 125, 0);
+
+        // this.imgHeight = this.image.nativeElement.offsetHeight;
+        // this.imgOffset = this.imgHeight + 10;
+  
+        this.buttonContainer.nativeElement.style.top = height + this.imgHeight - 15 + 'px';
       }
-      let height = Math.max(window.pageYOffset - 125, 0);
-      this.heightBalancer.nativeElement.style.height = height + 'px';
-      this.buttonContainer.nativeElement.style.top = height + this.imgHeight - 15 + 'px';
     });
   }
 
@@ -162,12 +215,12 @@ export class InstructionsComponent implements OnInit {
     this.image.nativeElement.setAttribute("src", src);
     this.ref.nativeElement.setAttribute("href", src);
 
-    let height = Math.max(window.pageYOffset - 125, 0);
     if(this.toggleMessage == "Hide Image") {
-      this.imgHeight = this.image.nativeElement.offsetHeight;
-      this.imgOffset = this.imgHeight + 10;
+      let height = Math.max(this.currentScrollPos - 125, 0);
+
+      this.buttonContainer.nativeElement.style.top = height + this.imgHeight - 15 + 'px';
     }
-    this.buttonContainer.nativeElement.style.top = height + this.imgHeight - 15 + 'px';
+    
   }
 
   toggleImage() {
